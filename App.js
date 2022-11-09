7eb4c3fbcfb959f68ae02be6f7c3c740b5248e87
Benco DEVOPS <codebrojects@gmail.com>
2022-11-09 18:38:29 +0200

    TailWind css

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { Text, View } from 'react-native';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>BENCO HA GEVER</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>qweqweqweqwe</Text>
       <StatusBar style='auto' />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
