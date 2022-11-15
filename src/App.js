81ba16dbc466591e5e07bab8047edef54f845fc5
Benos <benhanover4@gmail.com>
2022-11-15 18:36:31 +0200

    Redux

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+// import store from './redux/store';
+
+export default function App() {
+  return (
+    // <Provider store={store}>
+    <View className='items-center justify-center flex-1 bg-white'>
+      <Text>Benco ha gever</Text>
+    </View>
+    // </Provider>
+  );
+}
